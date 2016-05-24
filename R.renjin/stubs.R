384b23cbe5bc37e750afee7094e17d47a4c032f4
Alex Bertram <alex@bedatadriven.com>
2016-05-24 09:23:15 +0200

    Stubbed out Rcpp.plugin.maker for RcppArmadillo

--- R.renjin/stubs.R
+++ R.renjin/stubs.R
@@ -3,3 +3,10 @@
 evalCpp <- function(...) {
     stop("evalCpp is not supported by Renjin at this time.")
 }
+
+
+Rcpp.plugin.maker <- function(...) {
+    function(...) {
+        stop("inline c++ compilation not supported by Renjin at this time.")
+    }
+}
